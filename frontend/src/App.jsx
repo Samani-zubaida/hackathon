dfb52bc3da8c05a5421e057ed1f5eb846ea31301
Zubaida-Samani <zubaidasamani97@gmail.com>
2025-12-06 21:10:36 +0530

    ai change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
-import "./App.css";
+// import "./App.css";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import { Toaster } from "react-hot-toast";
 import ChatCompo from "./component/chatCompo.jsx";
 import { ChatProvider } from "./Context/chatContext.jsx";
-
+import Layout from "./components/Layout/Layout"
 function App() {
   return (
     <ChatProvider>
@@ -17,29 +16,11 @@ function App() {
 
           {/* AI Chat Route */}
           <Route path="/chatAI" element={<ChatCompo />} />
+           <Route path="/map" element={<Layout/>} />
         </Routes>
       </div>
     </ChatProvider>
   );
 }
 
-export default App;
-
-
-=======
-import Layout from "./components/Layout/Layout"
-
-
-
-function App() {
- 
-
-  return (
-    <>
-       <Layout />
-    </>
-  )
-}
-
 export default App
->>>>>>> aead57f0618b75dc13a04f6dd98d9c0bc97f1742
