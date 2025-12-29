b32b17b9b7a3ab56fcfeb9d6ee3c2bd91aeef664
Zubaida-Samani <zubaidasamani97@gmail.com>
2025-12-29 15:44:31 +0530

    chatbot commit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,9 +13,20 @@ const Login = () => {
 
         {/* Overlay Content */}
         <div className="absolute inset-0 flex justify-center items-center">
-          <div className="bg-white bg-opacity-70 text-black p-6 rounded-xl shadow-lg">
-            this is the div
-          </div>
+         <div className="min-h-screen flex items-center justify-center bg-gray-100">
+  <div className="flex space-x-4 bg-white bg-opacity-70 p-6 rounded-xl shadow-lg">
+    
+    <div className="bg-white bg-opacity-70 text-black p-6 rounded-xl shadow-lg">
+      Left Box
+    </div>
+
+    <div className="bg-white bg-opacity-70 text-black p-6 rounded-xl shadow-lg">
+      Right Box
+    </div>
+
+  </div>
+</div>
+
         </div>
       </div>
     </>
